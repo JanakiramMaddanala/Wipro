d18ab6440e0c555a1ae19c1d607f3ef80feaa0bd
Maddanala Janakiram <ram.rjy09@gmail.com>
2017-08-17 12:44:27 +0530

    Adding Wipro solution

---
+++ WiproQuestions/WiproQuestions/WiproQuestions/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WiproQuestions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
