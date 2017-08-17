4c9d0c6da94f4c48ec688cd687db898d227199a0
Maddanala Janakiram <ram.rjy09@gmail.com>
2017-08-17 14:32:05 +0530

    Added Four Wipro Programs

--- WiproQuestions/WiproQuestions/WiproQuestions/Program.cs
+++ WiproQuestions/WiproQuestions/WiproQuestions/Program.cs
@@ -10,6 +10,171 @@ namespace WiproQuestions
     {
         static void Main(string[] args)
         {
+            #region EncodedArray
+            Console.WriteLine("Encoded Array");
+            Console.WriteLine("--------------");
+            var encodedArray = new[] { -2, -7, 12, -15 };
+            int arrayLength = encodedArray.Length;
+            FindOriginalFirstAndSum(encodedArray, arrayLength);
+            Console.WriteLine("--------------");
+            Console.WriteLine();
+            Console.WriteLine();
+            #endregion
+
+            #region PIN
+            Console.WriteLine("PIN Generation");
+            Console.WriteLine("--------------");
+            int alpha = 0, beta = 0, gamma = 1024;
+            int pin = CreatePIN(alpha, beta, gamma);
+            Console.WriteLine(pin);
+            Console.WriteLine("--------------");
+            Console.WriteLine();
+            Console.WriteLine();
+            #endregion
+
+            #region SumOfSumsOfDigits
+            Console.WriteLine("SumOfSumsOfDigits");
+            Console.WriteLine("--------------");
+            int input = 582109;
+            int result = SumOfSumsOfDigits(input);
+            Console.WriteLine($"Sum Of Sums Of Digits : {result}");
+            Console.WriteLine("--------------");
+            Console.WriteLine();
+            Console.WriteLine();
+            #endregion
+
+            #region Palindrome
+            Console.WriteLine("Palindrome");
+            Console.WriteLine("----------");
+            string word = "mAlaYalAm";
+            int palindromeValue = IsPalindrome(word);
+            Console.WriteLine($"Palindrome Value: {palindromeValue}");
+            Console.WriteLine("----------");
+            Console.WriteLine();
+            Console.WriteLine();
+            #endregion
+
+            Console.ReadLine();
+        }
+
+        private static int IsPalindrome(string word)
+        {
+            var wordCharArray = word.ToUpper().ToCharArray();
+            var reverseWordCharArray = wordCharArray.Reverse();
+
+            if(wordCharArray.SequenceEqual(reverseWordCharArray))
+            {
+                return 1;
+            }
+            return 2;
+        }
+
+        private static int SumOfSumsOfDigits(int input)
+        {
+            var inputCharArray = input.ToString().ToCharArray();
+            int length = inputCharArray.Length;
+            int sum = 0;
+            for (int i = 0; i < (inputCharArray.Length); i++)
+            {
+                int value = Convert.ToInt32(inputCharArray[i].ToString());
+                sum = sum + (length * value);
+                length--;
+            }
+            return sum;
+        }
+
+        private static int CreatePIN(int alpha, int beta, int gamma)
+        {
+            string alphaString = GetAlpha(alpha);
+            string betaString = GetBeta(beta);
+            string gammaString = GetGamma(gamma);
+
+            string pin = string.Concat(gammaString, betaString, alphaString);
+
+            return Convert.ToInt32(pin);
+        }
+
+        private static string GetGamma(int gamma)
+        {
+            var gammaCharArray = gamma.ToString().ToCharArray();
+            int length = gammaCharArray.Length;
+            int hundredsIndex = length - 3;
+            int unitsIndex = length - 1;
+            char value;
+            if(gamma <= 99)
+            {
+                value = gammaCharArray[unitsIndex];
+                return value == '0' ? "9" : value.ToString(); 
+            }
+            else
+            {
+                value = gammaCharArray[hundredsIndex];
+                return value == '0' ? "9" : value.ToString();
+            }
+        }
+
+        private static string GetBeta(int beta)
+        {
+            var betaCharArray = beta.ToString().ToCharArray();
+            int tensIndex = betaCharArray.Length - 2;
+            if(betaCharArray.Length == 1)
+            {
+                return betaCharArray[0].ToString();
+            }
+
+            return betaCharArray[tensIndex].ToString();
+        }
+
+        private static string GetAlpha(int alpha)
+        {
+            string alphaString = alpha.ToString().ToCharArray().Last().ToString();
+
+            return alphaString;
+
+        }
+
+        private static Result FindOriginalFirstAndSum(int[] input1,int input2)
+        {
+            int[] encodedArray = new int[input2];
+            input1.CopyTo(encodedArray, 0);
+            
+            int index = (input2 - 2);
+
+            for (int i = index; i >= 0; i--)
+            {
+                input1[i] = input1[i] - input1[i + 1];
+            }
+
+            for (int i = 0; i < input2; i++)
+            {
+                Console.WriteLine($"Original : {input1[i]}, Encoded : {encodedArray[i]}");
+            }
+
+            Result result = new Result();
+            result.output1 = input1[0];
+            result.output2 = SumOriginalArray(input1);
+
+            Console.WriteLine($"Frist Number: {result.output1}");
+            Console.WriteLine($"Sum : {result.output2}");
+
+            return result;
+        }
+
+        private static int SumOriginalArray(int[] input1)
+        {
+            int sum = 0;
+            for (int i = 0; i < input1.Length; i++)
+            {
+                sum = sum + input1[i];
+            }
+
+            return sum;
         }
     }
+
+    public struct Result
+    {
+        public int output1;
+        public int output2;
+    };
 }
